daadcc96b392b41ad05066116e2ca2afd17d3864
annisana12 <98462398+annisana12@users.noreply.github.com>
2023-01-10 01:42:26 +0700

    homepage responsive

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import './HomePage.css';
-import Pose10 from '../../asset/HomePage/Pose10.png';
+import Pose10 from '../../asset/HomePage/Pose10.svg';
 import Navbar from '../General/Navbar';
-import Searchbox from '../General/Searchbox';
 import CategoryListHome from './CategoryListHome';
 import TopRatedServices from './TopRatedServices';
-import Feature from '../../asset/HomePage/Feature.png';
+import Feature from '../../asset/HomePage/Feature.svg';
 import { Link, useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import HomeSearchResult from './HomeSearchResult';
 
 const HomePage = () => {
     const navigate = useNavigate();
     const { isLoggedIn, user } = useSelector(state => state.auth);
+    const [srchResult, setSrchResult] = useState([]);
+    const [searchkey, setSearchkey] = useState('');
+    const [srchMsg, setSrchMsg] = useState('');
 
     const toApplySellerPage = () => {
         if (isLoggedIn) {
@@ -59,6 +62,230 @@ const HomePage = () => {
         }
     }
 
+    const service = [
+        {
+            "serviceId": 2,
+            "sellerId": 2,
+            "categoryId": 2,
+            "title": "Web Application",
+            "description": "Desain mantap harga Top",
+            "slug": "desain-art-1672662557514",
+            "createdAt": "2023-01-02T12:29:17.514Z",
+            "updatedAt": "2023-01-02T12:29:17.514Z",
+            firstName: 'Ahmad',
+            lastName: 'Na Jaemin',
+            rating: 4.9,
+            noOfBuyer: 79,
+            startingPrice: 59999,
+            "ServiceImages": [
+                {
+                    "image": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662557528.png"
+                }
+            ],
+            "Seller": {
+                "sellerId": 2,
+                "userId": 2,
+                "photoProfile": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662543893.png",
+                "description": "Penyedia desain Website untuk para UMKM",
+                "noRekening": "0123123123",
+                "bankName": "BCA",
+                "cardHolder": "Yudhitia",
+                "isVerified": 1,
+                "createdAt": "2023-01-02T12:29:03.916Z",
+                "updatedAt": "2023-01-08T17:48:25.506Z"
+            }
+        },
+        {
+            "serviceId": 1,
+            "sellerId": 2,
+            "categoryId": 2,
+            "title": "Mobile application",
+            "description": "Desain mantap harga Top",
+            "slug": "desain-art-1672662557514",
+            "createdAt": "2023-01-02T12:29:17.514Z",
+            "updatedAt": "2023-01-02T12:29:17.514Z",
+            firstName: 'Ahmad',
+            lastName: 'Na Jaemin',
+            rating: 4.9,
+            noOfBuyer: 79,
+            startingPrice: 60000,
+            "ServiceImages": [
+                {
+                    "image": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662557528.png"
+                }
+            ],
+            "Seller": {
+                "sellerId": 2,
+                "userId": 2,
+                "photoProfile": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662543893.png",
+                "description": "Penyedia desain Website untuk para UMKM",
+                "noRekening": "0123123123",
+                "bankName": "BCA",
+                "cardHolder": "Yudhitia",
+                "isVerified": 1,
+                "createdAt": "2023-01-02T12:29:03.916Z",
+                "updatedAt": "2023-01-08T17:48:25.506Z"
+            }
+        },
+        {
+            "serviceId": 5,
+            "sellerId": 2,
+            "categoryId": 2,
+            "title": "Book Cover Design",
+            "description": "Desain mantap harga Top",
+            "slug": "desain-art-1672662557514",
+            "createdAt": "2023-01-02T12:29:17.514Z",
+            "updatedAt": "2023-01-02T12:29:17.514Z",
+            firstName: 'Ahmad',
+            lastName: 'Na Jaemin',
+            rating: 4.9,
+            noOfBuyer: 79,
+            startingPrice: 79999,
+            "ServiceImages": [
+                {
+                    "image": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662557528.png"
+                }
+            ],
+            "Seller": {
+                "sellerId": 2,
+                "userId": 2,
+                "photoProfile": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662543893.png",
+                "description": "Penyedia desain Website untuk para UMKM",
+                "noRekening": "0123123123",
+                "bankName": "BCA",
+                "cardHolder": "Yudhitia",
+                "isVerified": 1,
+                "createdAt": "2023-01-02T12:29:03.916Z",
+                "updatedAt": "2023-01-08T17:48:25.506Z"
+            }
+        },
+        {
+            "serviceId": 3,
+            "sellerId": 2,
+            "categoryId": 2,
+            "title": "Logo Design",
+            "description": "Desain mantap harga Top",
+            "slug": "desain-art-1672662557514",
+            "createdAt": "2023-01-02T12:29:17.514Z",
+            "updatedAt": "2023-01-02T12:29:17.514Z",
+            firstName: 'Ahmad',
+            lastName: 'Na Jaemin',
+            rating: 5,
+            noOfBuyer: 93,
+            startingPrice: 80000,
+            "ServiceImages": [
+                {
+                    "image": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662557528.png"
+                }
+            ],
+            "Seller": {
+                "sellerId": 2,
+                "userId": 2,
+                "photoProfile": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662543893.png",
+                "description": "Penyedia desain Website untuk para UMKM",
+                "noRekening": "0123123123",
+                "bankName": "BCA",
+                "cardHolder": "Yudhitia",
+                "isVerified": 1,
+                "createdAt": "2023-01-02T12:29:03.916Z",
+                "updatedAt": "2023-01-08T17:48:25.506Z"
+            }
+        },
+        {
+            "serviceId": 9,
+            "sellerId": 2,
+            "categoryId": 2,
+            "title": "Brocure Design",
+            "description": "Desain mantap harga Top",
+            "slug": "desain-art-1672662557514",
+            "createdAt": "2023-01-02T12:29:17.514Z",
+            "updatedAt": "2023-01-02T12:29:17.514Z",
+            firstName: 'Ahmad',
+            lastName: 'Na Jaemin',
+            rating: 4.9,
+            noOfBuyer: 38,
+            startingPrice: 80000,
+            "ServiceImages": [
+                {
+                    "image": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662557528.png"
+                }
+            ],
+            "Seller": {
+                "sellerId": 2,
+                "userId": 2,
+                "photoProfile": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662543893.png",
+                "description": "Penyedia desain Website untuk para UMKM",
+                "noRekening": "0123123123",
+                "bankName": "BCA",
+                "cardHolder": "Yudhitia",
+                "isVerified": 1,
+                "createdAt": "2023-01-02T12:29:03.916Z",
+                "updatedAt": "2023-01-08T17:48:25.506Z"
+            }
+        },
+        {
+            "serviceId": 8,
+            "sellerId": 2,
+            "categoryId": 2,
+            "title": "UI/UX Design",
+            "description": "Desain mantap harga Top",
+            "slug": "desain-art-1672662557514",
+            "createdAt": "2023-01-02T12:29:17.514Z",
+            "updatedAt": "2023-01-02T12:29:17.514Z",
+            firstName: 'Ahmad',
+            lastName: 'Na Jaemin',
+            rating: 4.8,
+            noOfBuyer: 37,
+            startingPrice: 80000,
+            "ServiceImages": [
+                {
+                    "image": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662557528.png"
+                }
+            ],
+            "Seller": {
+                "sellerId": 2,
+                "userId": 2,
+                "photoProfile": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662543893.png",
+                "description": "Penyedia desain Website untuk para UMKM",
+                "noRekening": "0123123123",
+                "bankName": "BCA",
+                "cardHolder": "Yudhitia",
+                "isVerified": 1,
+                "createdAt": "2023-01-02T12:29:03.916Z",
+                "updatedAt": "2023-01-08T17:48:25.506Z"
+            }
+        }
+    ]
+
+    useEffect(() => {
+        if(!searchkey) {
+            setSrchResult([]);
+            setSrchMsg('');
+            document.getElementById('srchresult').style.display = 'none';
+        }
+    }, [searchkey]);
+
+    const regexSearch = new RegExp(`.*(${searchkey.toUpperCase()}).*`);
+    
+    const handleSearch = () => {
+        if(searchkey) {
+            const filtered = service.filter(item => regexSearch.exec(item.title.toUpperCase()));
+            setSrchResult(filtered);
+            
+            if (filtered.length === 0) {
+                setSrchMsg('Data not found.')
+            } else {
+                setSrchMsg('');
+            }
+
+            document.getElementById('srchresult').style.display = 'flex';
+        } else {
+            setSrchResult([]);
+            setSrchMsg('');
+            document.getElementById('srchresult').style.display = 'none';
+        }
+    };
+
     return (
         <div>
             <div className='top-container'>
@@ -72,10 +299,18 @@ const HomePage = () => {
                         <div className='text1-2'>Get The Best IT Service Only In Create IT!</div>
                     </div>
                     <div className='searchbox-cntr'>
-                        <Searchbox />
+                        <div className="search-container1">
+                            <input type="text" placeholder="Search..." value={searchkey} onChange={(event) => { setSearchkey(event.target.value) }} className="search-input1" />
+                            <div type="button" className="search-button1" onClick={handleSearch}>Search</div>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <div id='srchresult' style={{'display': 'none'}} className='srchresult-cntr'>
+                <HomeSearchResult data={srchResult} message={srchMsg}/>
+            </div>
+
             <div className='textcategory-container'>
                 <div className='text2-1'>Category List</div>
                 <div className='text2-2'>Here Is The List Of Services Category We Have, Look It Up!</div>
---
+++ src/components/Home/HomeSearchResult.jsx
@@ -0,0 +1,43 @@
+import { Link } from 'react-router-dom';
+import './HomeSearchResult.css';
+
+const HomeSearchResult = ({ data, message }) => {
+    return (
+        <div>
+            {message && <div className='searchhome-msg'>{message}</div>}
+
+            <div className='srchhomeresult-cntr'>
+                {data.map(item => (
+                    <div className="serviceslist22-box" key={`id-${item.serviceId}`}>
+                        <Link className="service1imgcntr22" to={`/service/${item.slug}`}>
+                            <img src={item.ServiceImages[0].image} alt='' className="servicelist22-img"></img>
+                            <div className='toprated-ratebuy22'>
+                                <div><i className='bx bx-star'></i>{item.rating}</div>
+                                <div><i className='bx bx-group'></i>{item.noOfBuyer}</div>
+                            </div>
+                        </Link>
+                        <div className="servicelist22-info">
+                            <div className="service22-info1">
+                                <div className="service22-info2">
+                                    <img src={item.Seller.photoProfile} alt=''></img>
+                                    <Link to={`/seller/${item.sellerId}`} className='nav-link'>{item.firstName} {item.lastName}</Link>
+                                </div>
+                                <Link to={`/service/${item.slug}`} className="service22-info3 nav-link">{item.title}</Link>
+                            </div>
+                            <div className="service22-info4">
+                                <i className='bx bx-dollar-circle'></i>
+                                {(item.startingPrice % 1000 === 0) ? (
+                                    <span>Start from Rp {item.startingPrice / 1000}.000,-</span>
+                                ) : (
+                                    <span>Start from Rp {item.startingPrice / 1000},-</span>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default HomeSearchResult;
