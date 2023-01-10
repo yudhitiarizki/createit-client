a647bca98bc5120818111c7cad28901b533246a3
annisana12 <98462398+annisana12@users.noreply.github.com>
2023-01-10 10:02:43 +0700

    service list/category responsive + searchbox berfungsi

--- src/components/Services/ServicePerCategory.jsx
+++ src/components/Services/ServicePerCategory.jsx
@@ -1,23 +1,110 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './ServicePerCategory.css';
 import '../Home/HomePage.css';
 import Navbar from '../General/Navbar';
-import Searchbox from '../General/Searchbox';
 import Pose10 from '../../asset/HomePage/Pose10.svg';
 import { Link, useParams } from 'react-router-dom';
 import ServiceListCategory from './ServicesListCategory';
 import { getCategory } from '../../redux/actions/category';
+import ServiceSearchResult from './ServiceSearchResult';
 
 const ServicePerCategory = () => {
     const dispatch = useDispatch();
     const { id } = useParams();
 
     const category = useSelector(state => state.category);
+    const [srchResult, setSrchResult] = useState([]);
+    const [searchkey, setSearchkey] = useState('');
+    const [srchMsg, setSrchMsg] = useState('');
 
     useEffect(() => {
         dispatch(getCategory());
     }, [dispatch]);
+
+    const service = [
+        {
+            "serviceId": 2,
+            "sellerId": 2,
+            "image": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662557528.png",
+            "firstName": "Yudhit",
+            "lastName": "Rizki",
+            "photoProfile": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662543893.png",
+            "title": "Logo Design",
+            "rating": 4.9,
+            "noOfBuyer": 0,
+            "startingPrice": 40000,
+            "slug": "desain-art-1672662557514"
+        },
+        {
+            "serviceId": 3,
+            "sellerId": 2,
+            "image": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662557528.png",
+            "firstName": "Yudhit",
+            "lastName": "Rizki",
+            "photoProfile": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662543893.png",
+            "title": "Book Cover Design",
+            "rating": 4.9,
+            "noOfBuyer": 0,
+            "startingPrice": 49999,
+            "slug": "desain-art-1672662557514"
+        },
+        {
+            "serviceId": 5,
+            "sellerId": 2,
+            "image": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662557528.png",
+            "firstName": "Yudhit",
+            "lastName": "Rizki",
+            "photoProfile": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662543893.png",
+            "title": "Poster design",
+            "rating": 4.9,
+            "noOfBuyer": 0,
+            "startingPrice": 70000,
+            "slug": "desain-art-1672662557514"
+        },
+        {
+            "serviceId": 4,
+            "sellerId": 2,
+            "image": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662557528.png",
+            "firstName": "Yudhit",
+            "lastName": "Rizki",
+            "photoProfile": "http://coal-jolly-single.glitch.me/public/uploads/images/images-1672662543893.png",
+            "title": "Logo Design",
+            "rating": 4.9,
+            "noOfBuyer": 0,
+            "startingPrice": 73582,
+            "slug": "desain-art-1672662557514"
+        }
+    ]
+
+    useEffect(() => {
+        if (!searchkey) {
+            setSrchResult([]);
+            setSrchMsg('');
+            document.getElementById('srchresult2').style.display = 'none';
+        }
+    }, [searchkey]);
+
+    const regexSearch = new RegExp(`.*(${searchkey.toUpperCase()}).*`);
+
+    const handleSearch = () => {
+        if (searchkey) {
+            const filtered = service.filter(item => regexSearch.exec(item.title.toUpperCase()));
+            setSrchResult(filtered);
+
+            if (filtered.length === 0) {
+                setSrchMsg('Data not found.')
+            } else {
+                setSrchMsg('');
+            }
+
+            document.getElementById('srchresult2').style.display = 'flex';
+        } else {
+            setSrchResult([]);
+            setSrchMsg('');
+            document.getElementById('srchresult2').style.display = 'none';
+        }
+    };
 
     return (
         <div>
@@ -37,24 +124,34 @@ const ServicePerCategory = () => {
                         <div className='text1-21'>Get The Best IT Service Only In Create IT!</div>
                     </div>
                     <div className='searchbox-cntr1'>
-                        <Searchbox />
+                        <div className="search-container1">
+                            <input type="text" placeholder="Search..." value={searchkey} onChange={(event) => { setSearchkey(event.target.value) }} className="search-input1" />
+                            <div type="button" className="search-button1" onClick={handleSearch}>Search</div>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <div id='srchresult2' style={{ 'display': 'none' }} className='srchresult-cntr'>
+                <ServiceSearchResult data={srchResult} message={srchMsg} />
+            </div>
+
             <div className='textcategory-container'>
                 <div className='text2-1'>All In One IT Solution</div>
                 <div className='text2-2'>We can help with all your needs. Over {category.length} service categories to choose from.</div>
             </div>
-            <div className='categorylist1'>
-                {category.map((category) => (category.categoryId === +id) ? (
-                    <div className='categorybox1 catbox1' key={`id-${category.categoryId}`}>
-                        {category.category}
-                    </div>
-                ) : (
-                    <div className='categorybox1 catbox2' key={`id-${category.categoryId}`}>
-                        <Link to={`/category/${category.categoryId}`} className='nav-link'>{category.category}</Link>
-                    </div>
-                ))}
+            <div className='catlist-cntr11'>
+                <div className='categorylist1'>
+                    {category.map((category) => (category.categoryId === +id) ? (
+                        <div className='categorybox1 catbox1' key={`id-${category.categoryId}`}>
+                            {category.category}
+                        </div>
+                    ) : (
+                        <div className='categorybox1 catbox2' key={`id-${category.categoryId}`}>
+                            <Link to={`/category/${category.categoryId}`} className='nav-link'>{category.category}</Link>
+                        </div>
+                    ))}
+                </div>
             </div>
             <ServiceListCategory />
         </div>
--- src/components/Services/ServicesListCategory.jsx
+++ src/components/Services/ServicesListCategory.jsx
@@ -2,6 +2,7 @@ import { React, useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
 import { Link, useParams } from "react-router-dom";
 import './ServicesListCategory.css';
+import '../Home/HomeSearchResult.css';
 import { useSelector, useDispatch } from "react-redux";
 import { getServiceByCategory } from "../../redux/actions/service";
 
@@ -34,44 +35,41 @@ const ServiceListCategory = () => {
     return (
         <div className="servicelists11">
             <div className="servicelist11-cntr">
-                { service.length ? ( 
-                <>
-                    { currentItems.map(item => (
-                        <div className="serviceslist11-box" key={`id-${item.serviceId}`}>
-                            {/* item.id nanti diganti jadi item.serviceId */}
-                            <Link className="service1imgcntr1" to={`/service/${item.slug}`}>
-                                {/* nanti diganti pakai image dari item */}
-                                <img src={item.image} alt='' className="servicelist11-img"></img>
-                                <div className='toprated-ratebuy11'>
-                                    {/* masih data dummy */}
-                                    <div><i className='bx bx-star'></i>{item.rating ? item.rating : 0}</div>
-                                    <div><i className='bx bx-group'></i>{item.noOfBuyer}</div>
-                                </div>
-                            </Link>
-                            <div className="servicelist11-info">
-                                <div className="service11-info1">
-                                    <div className="service11-info2">
-                                        {/* masih data dummy */}
-                                        <img src={item.photoProfile} alt=''></img>
-                                        {/* nanti ganti sellerId */}
-                                        <Link to={`/seller/${item.sellerId}`} className='nav-link'>{item.firstName} {item.lastName}</Link>
+                {service.length ? (
+                    <>
+                        {currentItems.map(item => (
+                            <div className="serviceslist22-box" key={`id-${item.serviceId}`}>
+                                <Link className="service1imgcntr22" to={`/service/${item.slug}`}>
+                                    <img src={item.image} alt='' className="servicelist22-img"></img>
+                                    <div className='toprated-ratebuy22'>
+                                        <div><i className='bx bx-star'></i>{item.rating}</div>
+                                        <div><i className='bx bx-group'></i>{item.noOfBuyer}</div>
                                     </div>
-                                    {/* nanti ganti jadi serviceId */}
-                                    <Link to={`/service/${item.slug}`} className="service11-info3 nav-link">{item.title}</Link>
-                                </div>
-                                <div className="service11-info4">
-                                    <i className='bx bx-dollar-circle'></i>
-                                    {/* masih data dummy */}
-                                    <span>Start from Rp 20000</span>
+                                </Link>
+                                <div className="servicelist22-info">
+                                    <div className="service22-info1">
+                                        <div className="service22-info2">
+                                            <img src={item.photoProfile} alt=''></img>
+                                            <Link to={`/seller/${item.sellerId}`} className='nav-link'>{item.firstName} {item.lastName}</Link>
+                                        </div>
+                                        <Link to={`/service/${item.slug}`} className="service22-info3 nav-link">{item.title}</Link>
+                                    </div>
+                                    <div className="service22-info4">
+                                        <i className='bx bx-dollar-circle'></i>
+                                        {(item.startingPrice % 1000 === 0) ? (
+                                            <span>Start from Rp {item.startingPrice / 1000}.000,-</span>
+                                        ) : (
+                                            <span>Start from Rp {item.startingPrice / 1000},-</span>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
-                </>
+                        ))}
+                    </>
                 ) : (
                     <></>
-                ) }
-                
+                )}
+
             </div>
             <ReactPaginate
                 breakLabel="..."
