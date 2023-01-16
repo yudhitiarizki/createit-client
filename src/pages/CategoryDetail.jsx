cb21f94af277bb312c5b6d5644e36524bb47afa4
annisana12 <98462398+annisana12@users.noreply.github.com>
2023-01-16 11:08:55 +0700

    add something went wrong page

--- src/pages/CategoryDetail.jsx
+++ src/pages/CategoryDetail.jsx
@@ -5,34 +5,42 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getServiceByCategory } from '../redux/actions/service';
 import Loader from '../components/General/Loader';
-import { setToLoad, setToNotLoad } from '../redux/actions/loader';
+import { setToLoad, setToNotLoad, setWentWrong } from '../redux/actions/loader';
+import SomethingWrong from '../components/SomethingWrong/SomethingWrong';
 
 const CategoryDetail = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
 
-  const { isLoading } = useSelector(state => state.Loading);
-  
+  const { isLoading, isError } = useSelector(state => state.Loading);
+
   const { service } = useSelector(state => state.service);
 
   useEffect(() => {
     dispatch(setToLoad());
-    dispatch(getServiceByCategory(id)).then(() => {
-      dispatch(setToNotLoad());
-    });
+    dispatch(getServiceByCategory(id))
+      .then(() => {
+        dispatch(setToNotLoad());
+      })
+      .catch(error => {
+        dispatch(setWentWrong());
+      });
   }, [id, dispatch]);
 
   return (
     <>
       {isLoading ?
         <Loader />
-        :
-        <>
-          <ServicePerCategory service={service}/>
-          <div style={{ 'width': '100%', 'background': '#e5e5e533' }}>
-            <Footer />
-          </div>
-        </>
+        : (isError ?
+          <SomethingWrong />
+          :
+          <>
+            <ServicePerCategory service={service} />
+            <div style={{ 'width': '100%', 'background': '#e5e5e533' }}>
+              <Footer />
+            </div>
+          </>
+        )
       }
     </>
   );
--- src/redux/reducers/loading.js
+++ src/redux/reducers/loading.js
@@ -1,10 +1,12 @@
 import {
     SET_TO_LOADING,
-    SET_TO_NOT_LOADING
+    SET_TO_NOT_LOADING,
+    SOMETHING_WENT_WRONG
 } from '../actions/types';
 
 const initialState = {
-    isLoading: false
+    isLoading: false,
+    isError: false
 };
 
 const setLoading = (state = initialState, action) => {
@@ -14,12 +16,20 @@ const setLoading = (state = initialState, action) => {
         case SET_TO_LOADING:
             return {
                 ...state,
-                isLoading: true
+                isLoading: true,
+                isError: false
             }
         case SET_TO_NOT_LOADING:
             return {
                 ...state,
-                isLoading: false
+                isLoading: false,
+                isError: false
+            }
+        case SOMETHING_WENT_WRONG:
+            return {
+                ...state,
+                isLoading: false,
+                isError: true
             }
         default:
             return state
