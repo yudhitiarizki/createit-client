1964e4c58fa66e7655eaa33f15b1c4126c50d833
annisana12 <98462398+annisana12@users.noreply.github.com>
2023-01-22 01:25:31 +0700

    style list inbox done

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import Footer from '../components/General/Footer';
-import Ellipse2 from '../asset/Navbar/Ellipse2.png';
 import ChatRoom from '../components/Chat/ChatRoom';
 
-const Inbox = () => {
-    const [lastData, setLastData] = useState([]);
+import Ellipse2 from '../asset/Navbar/Ellipse2.png';
+import '../components/Chat/Inbox.css';
 
-    const room = [
+const Inbox = () => {
+    // const {user} = useSelector(state => state.auth);
+    const user = useMemo(() => ({ userId: 1 }), []); //data dummy
+
+    const [lastData, setLastData] = useState([]);
+    const [message, setMessage] = useState('');
+
+    const room = useMemo(() => [
         {
             roomId: 1,
             name: 'room 1',
@@ -75,33 +83,76 @@ const Inbox = () => {
                 }
             ]
         }
-    ];
+    ], []);
+
+    const filterRoom = useCallback(() => {
+        let filteredData = [];
+        room.forEach(room => {
+            const messageLength = room.Messages.length;
+            const lastMessage = room.Messages[messageLength - 1];
+
+            const lastUser = room.RoomParticipants.filter(person => person.userId !== user.userId)[0];
+            const result = {
+                roomId: room.roomId,
+                lastUser: lastUser,
+                lastMessage: lastMessage
+            }
+
+            filteredData = [...filteredData, result]
+        })
+
+        setLastData(filteredData);
+    }, [room, user]);
+
+    useEffect(() => {
+        if (room.length) {
+            filterRoom();
+            setMessage('');
+        } else {
+            setMessage('There is currently no chat.')
+        }
+    }, [filterRoom, room]);
+
+    const getTime = useCallback((data) => {
+        const date = new Date(data);
+        const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+
+        return date.toLocaleDateString('id-ID', options);
+    }, []);
+
+    const handleRoomDetail = useCallback((roomId) => {
+        const selectedRoom = room.filter(room => room.roomId === roomId);
+
+        // dispatch pakai payload selectedRoom
+
+    }, [room]);
 
     return (
         <>
-            <div>
-                <div>
-                    <div>Inbox</div>
-                    <div>
-                        {room.map(room => (
-                            <div key={`id-${room.roomId}`}>
-                                <div>
-                                    <img alt={1}></img>
+            <div className='inbox-container'>
+                <ChatRoom />
+
+                <div className='inboxlist-cntr'>
+                    <div className='inbox-header'>Inbox</div>
+                    <div className='lastmsg-cntr'>
+                        {room.length &&
+                            lastData.map(({ roomId, lastUser, lastMessage }) => (
+                                <div key={`id-${roomId}`} className='lastmsg-box'>
+                                    <img src={lastUser.photoProfile} alt={1} className='lastmsg-photo'></img>
+                                    <div className='lastmsg-center'>
+                                        <div className='lastmsg-time'>{getTime(lastMessage.createdAt)}</div>
+                                        <div className='lastmsg-name'>{lastUser.firstName} {lastUser.lastName}</div>
+                                        <div className='lastmsg-msg'>{lastMessage.message}</div>
+                                    </div>
+                                    <div onClick={() => { handleRoomDetail(roomId) }} className='lastmsg-det-btn'>
+                                        <i className='bx bx-right-arrow-alt'></i>
+                                    </div>
                                 </div>
-                                <div>
-                                    <div></div>
-                                    <div></div>
-                                    <div></div>
-                                </div>
-                                <div>
-                                    <i className='bx bx-right-arrow-alt'></i>
-                                </div>
-                            </div>
-                        ))}
+                            ))
+                        }
+                        {message && <div>{message}</div>}
                     </div>
                 </div>
-
-                <ChatRoom />
             </div>
 
             <Footer />
